b6378aca6992b86fab8e71bbcc12ad2943219a65
Sarsande-Sunil <priyankaingle250@gmail.com>
2022-12-22 20:08:17 +0530

    food-odering-app

--- project-priyanka/src/pages/Home.jsx
+++ project-priyanka/src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import React from 'react'; 
+import heroImg from "../assets/images/hero.png"
+import Helmet from '../components/Helmet/Helmet';
+import{Container,Row,Col} from "reactstrap"
+import style from "../styles/herosection.css"
 const Home = () => {
   return (
-    <div>Home</div>
+    <Helmet title="Home">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className='hero_content'>
+                <h5 className='mb-3'>Easy make to make an order</h5>
+                <h1 className='mb-4 hero_title'>
+                  <span>HUNGRY?</span> Just wait <br /> a food <span>at your door</span>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quae consequatur laborum voluptatibus modi enim aliquam porro velit amet odio ipsa
+                  culpa tenetur ad, minus accusamus sapiente aut totam quia voluptates!
+                </p>
+
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className='hero_image'>
+                <img src={heroImg} alt="hero_img"  className="w-100"/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
